8170925e43177bcf9226ec5b85dd7f962ae44272
Giomar Moreno <giomarmorenoll@gmail.com>
2021-10-06 17:52:41 -0500

    change initial smartsite value

--- src/Banner.js
+++ src/Banner.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import smartsite from './img/smartsite.png'
 
 const Alo = () => {
   return (
     <>
       <div className='banner'>
+        <img src={smartsite} width='150'/>
          <div>
             <h2>Página en Construcción</h2>
-            <p>El sitio web estará listo pronto</p>
+            <p>Muy pronto te enterarás de los servicios digitales que ofrecemos</p>
          </div> 
          <div class='progress-bar blue stripes shine'>
-            <span style={{width:'18%'}}></span>
-            <p>10%</p>
+            <span style={{width:'40%'}}></span>
+            <p>40%</p>
          </div>
          <ul className='red-social'>
               <li class="list-inline-item"><a href='#'><ion-icon name="logo-instagram"></ion-icon></a></li>
