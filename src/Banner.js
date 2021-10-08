844b174559ff4b7b0ff1ccd97cf80aebee4adac9
Giomar Moreno <giomarmorenoll@gmail.com>
2021-10-08 15:27:04 -0500

    change initial smartsite value

--- src/Banner.js
+++ src/Banner.js
@@ -5,7 +5,7 @@ const Alo = () => {
   return (
     <>
       <div className='banner'>
-        <img src={smartsite} width='150'/>
+        <img style={{paddingTop:'30px'}}src={smartsite} width='150'/>
          <div>
             <h2>Página en Construcción</h2>
             <p>Muy pronto te enterarás de los servicios digitales que ofrecemos</p>
@@ -17,10 +17,14 @@ const Alo = () => {
          <ul className='red-social'>
               <li class="list-inline-item"><a href='#'><ion-icon name="logo-instagram"></ion-icon></a></li>
               <li class="list-inline-item"><a href='#'><ion-icon name="logo-twitter"></ion-icon></a></li>
-              <li class="list-inline-item"><a href='#'><ion-icon name="logo-youtube"></ion-icon></a></li>
+              <li class="list-inline-item"><a href='https://www.youtube.com/watch?v=P4UGyADvWVU'target='_blank'><ion-icon name="logo-youtube"></ion-icon></a></li>
               <li class="list-inline-item"><a href='#'><ion-icon name="logo-discord"></ion-icon></a></li>
               <li class="list-inline-item"><a href='#'><ion-icon name="logo-facebook"></ion-icon></a></li>
          </ul>
+        <div className='foot'>
+           <p><ion-icon name='logo-whatsapp'></ion-icon>: 985847585</p>
+           <p><ion-icon name='mail-outline'></ion-icon>: smartsiteconstruction@gmail.com</p>
+        </div> 
       </div> 
     </>
   )
